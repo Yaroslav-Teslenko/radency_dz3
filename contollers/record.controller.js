82c90926731c287823407dbd88c945c176ac2012
Yaroslav Teslenko <teslenko.yaroslav21@gmail.com>
2021-10-12 16:12:23 +0300

    add: validations

--- contollers/record.controller.js
+++ contollers/record.controller.js
@@ -1,66 +1,96 @@
 const RecordService = require("../service/record.service");
+const ApiError = require("../service/api.error");
+
+let yup = require("yup");
+
+let schema = yup.object().shape({
+  name: yup.string().required(),
+  category: yup.string().required(),
+  content: yup.string().required(),
+  active: yup.boolean().default(true),
+  created: yup.date().default(function () {
+    return new Date();
+  }),
+});
+
 class RecordController {
-  async createRecord(req, res, next) {
+  createRecord(req, res, next) {
     try {
-      // console.log(req.body.name);
-      const payload = req.body;
-      const responce = await RecordService.createRecord(payload);
-      return res.json(responce);
+      {
+        const payload = req.body;
+        schema
+          .validate(payload, {
+            abortEarly: false,
+          })
+          .then(() => {
+            const responce = RecordService.createRecord(payload);
+            return res.json(responce);
+          })
+          .catch((err) => {
+            /*  err.name; // => 'ValidationError'
+                err.errors; // => ['Deve ser maior que 18'] */
+            next(ApiError.BadRequest("Ошибка при валидации", err.errors));
+          });
+      }
     } catch (e) {
+      console.log(e);
       next(e);
     }
   }
-  async getRecord(req, res, next) {
+  getRecord(req, res, next) {
     try {
       const id = req.params.id;
-      const responce = await RecordService.getRecord(id);
+      const responce = RecordService.getRecord(id);
       return res.json(responce);
     } catch (e) {
       next(e);
     }
   }
-  async editRecord(req, res, next) {
+  editRecord(req, res, next) {
+    const id = req.params.id;
+    const payload = req.body;
+    schema
+      .validate(payload, {
+        abortEarly: false,
+      })
+      .then(() => {
+        const responce = RecordService.editRecord(id, payload);
+        return res.json(responce);
+      })
+      .catch((err) => {
+        next(ApiError.BadRequest("Ошибка при валидации", err.errors));
+      });
+  }
+  deleteRecord(req, res, next) {
     try {
       // console.log(req);
       const id = req.params.id;
-      const payload = req.body;
-      console.log(id);
-      const responce = await RecordService.editRecord(id, payload);
+      const responce = RecordService.deleteRecord(id);
       return res.json(responce);
     } catch (e) {
       next(e);
     }
   }
-  async deleteRecord(req, res, next) {
-    try {
-      // console.log(req);
-      const id = req.params.id;
-      const responce = await RecordService.deleteRecord(id);
-      return res.json(responce);
-    } catch (e) {
-      next(e);
-    }
-  }
-  async setAcitiveToggle(req, res, next) {
+  setAcitiveToggle(req, res, next) {
     try {
       const id = req.params.id;
-      const responce = await RecordService.setAcitiveToggle(id);
+      const responce = RecordService.setAcitiveToggle(id);
       return res.json(responce);
     } catch (e) {
       next(e);
     }
   }
-  async getRecords(req, res, next) {
+  getRecords(req, res, next) {
     try {
-      const responce = await RecordService.getRecords();
+      const responce = RecordService.getRecords();
       return res.json(responce);
     } catch (e) {
       next(e);
     }
   }
-  async getSummaryCat(req, res, next) {
+  getSummaryCat(req, res, next) {
     try {
-      const responce = await RecordService.getSummaryCat();
+      const responce = RecordService.getSummaryCat();
       return res.json(responce);
     } catch (e) {
       next(e);
@@ -69,5 +99,5 @@ class RecordController {
 }
 
 /* const adminrRole = new Role({ value: "ADMIN" });
-      await adminrRole.save(); */
+       adminrRole.save(); */
 module.exports = new RecordController();
